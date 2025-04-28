a1efe5385c17ff3774940717b44574449e24e666
igjerapic24 <igjerapic@gmail.com>
2025-04-28 21:52:46 +0200

    create scripts for entire param space of two dims

--- Graphite/create_dirs.sh
+++ Graphite/create_dirs.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
-# For initial and with stricter convergence 
-ecut=40.0
-nk=4
-ntasks=12
-
-for dim_plane in $(cat lats_vert.txt); do 
-    for dim_vert in 
-for conv_thr in "1.0d-8" "1.0d-12"; do
-    bash create_dir.sh $ecut $nk $conv_thr
-done
-
-# for various ecut other than 40.0
-conv_thr="1.0d-8"
-for ecut in 20.0 30.0 50.0; do
-    bash create_dir.sh $ecut $nk $conv_thr
-done
-
-# for various k-points other than 4 
-ecut=40.0
-for nk in 2 6; do
-    bash create_dir.sh $ecut $nk $conv_thr
+# create directories based on the parameter space defined in text files
+for dim_plane in $(cat dims_plane.txt); do 
+    for dim_vert in $(cat dims_vert.txt); do
+        bash create_dir.sh $dim_plane $dim_vert 
+    done
 done
--- Graphite/create_inputs.sh
+++ Graphite/create_inputs.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-# For initial and with stricter convergence 
-ecut=40.0
-nk=4
-for conv_thr in "1.0d-8" "1.0d-12"; do
-    bash create_Si.scf.in_template.sh $ecut $nk $conv_thr
-done
+# create input files based on the parameter space defined in text files
 
-# for various ecut other than 40.0
-conv_thr="1.0d-8"
-for ecut in 20.0 30.0 50.0; do
-    bash create_Si.scf.in_template.sh $ecut $nk $conv_thr
-done
-
-# for various k-points other than 4 
-ecut=40.0
-for nk in 2 6; do
-    bash create_Si.scf.in_template.sh $ecut $nk $conv_thr
+for dim_plane in $(cat dims_plane.txt); do 
+    for dim_vert in $(cat dims_vert.txt); do
+        bash create_input.sh $dim_plane $dim_vert 
+    done
 done
